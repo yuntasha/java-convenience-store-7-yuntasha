2339ad37657a9d1a2762c0c24321d9f274163d1e
yuntasha <jaljayo85@gmail.com>
2024-11-10 00:10:57 +0900

    style(PromotionTest): remove unnecessary import

--- src/test/java/store/model/PromotionTest.java
+++ src/test/java/store/model/PromotionTest.java
@@ -1,12 +1,10 @@
 package store.model;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.FieldSource;
-import store.util.Validator;
 
 import java.time.LocalDate;
 import java.util.Arrays;
