ad1aef6b8fcc5ddd6ed22624ac37d15b24288884
yuntasha <jaljayo85@gmail.com>
2024-11-11 22:34:55 +0900

    style(Application): remove default annotation

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -8,7 +8,6 @@ import store.view.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         Convenience convenience = new Convenience();
         Receipt receipt = new CReceipt();
