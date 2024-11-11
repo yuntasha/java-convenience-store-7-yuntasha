fe461efd4fdd7941ed610c8ff5d839b7c913926e
yuntasha <jaljayo85@gmail.com>
2024-11-11 19:07:46 +0900

    style(BuyDto): write javadoc

--- src/main/java/store/dto/BuyDto.java
+++ src/main/java/store/dto/BuyDto.java
@@ -24,6 +24,15 @@ public class BuyDto {
      */
     private final int freeCount;
 
+    /**
+     * 프로모션이 적용되는 경우 사용
+     *
+     * @param name      제품 이름
+     * @param price     제품 가격
+     * @param count     일반 제품 개수
+     * @param pCount    프로모션 제품 개수
+     * @param freeCount 프로모션 증정품 개수
+     */
     public BuyDto(String name, int price, int count, int pCount, int freeCount) {
         this.name = name;
         this.price = price;
@@ -32,6 +41,13 @@ public class BuyDto {
         this.freeCount = freeCount;
     }
 
+    /**
+     * 프로모션이 미적용되는 경우 사용
+     *
+     * @param name
+     * @param price
+     * @param count
+     */
     public BuyDto(String name, int price, int count) {
         this.name = name;
         this.price = price;
@@ -40,26 +56,56 @@ public class BuyDto {
         this.freeCount = 0;
     }
 
+    /**
+     * 제품 이름 반환
+     *
+     * @return 제품 이름
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * 증정품 개수 반환
+     *
+     * @return 증정품 개수
+     */
     public int getFreeCount() {
         return freeCount;
     }
 
+    /**
+     * 해당 상품 총 개수 반환
+     *
+     * @return 총 개수
+     */
     public int getAllCount() {
         return count + pCount;
     }
 
+    /**
+     * 해당 제품 총 가격 반환
+     *
+     * @return 총 가격
+     */
     public int getAllPrice() {
         return getAllCount() * price;
     }
 
+    /**
+     * 프로모션으로 할인되는 가격 반환
+     *
+     * @return 프로모션으로 할인되는 가격
+     */
     public int getDiscount() {
         return freeCount * price;
     }
 
+    /**
+     * 프로모션 할인 적용 후 총 금액 반환
+     *
+     * @return 프로모션 할인 적용 후 총 금액
+     */
     public int getNoPromotionPrice() {
         return count * price;
     }
