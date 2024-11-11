bc6e08b9ed4e04bea164368161b6f5d34fe937f9
yuntasha <jaljayo85@gmail.com>
2024-11-11 19:28:59 +0900

    style(Convenience): write javadoc

--- src/main/java/store/model/Convenience.java
+++ src/main/java/store/model/Convenience.java
@@ -13,26 +13,70 @@ import java.util.List;
 import java.util.TreeMap;
 
 public class Convenience {
+    /**
+     * 편의점 제품
+     * <pre>
+     *     검색을 위해 Map으로 구성하였으며 보기 좋도록 사전순 정렬을 위해 TreeMap
+     * </pre>
+     */
     private TreeMap<String, Product> products = new TreeMap<>();
+    /**
+     * 편의점 프로모션
+     * <pre>
+     *     검색을 위해 Map으로 구성하였으며 보기 좋도록 사전순 정렬을 위해 TreeMap
+     * </pre>
+     */
     private TreeMap<String, Promotion> promotions = new TreeMap<>();
 
+    /**
+     * 프로모션과 제품 구성
+     *
+     * @param promotionLines 프로모션 정보
+     * @param productLines   제품 정보
+     */
     public void init(List<String> promotionLines, List<String> productLines) {
         addPromotions(promotionLines);
         addProducts(productLines);
     }
 
+    /**
+     * 제품 개수 반환
+     * <pre>
+     *     테스트를 위한 메서드
+     * </pre>
+     *
+     * @return 제품 개수 반환
+     */
     int getProductCount() {
         return products.size();
     }
 
+    /**
+     * 프로모션 개수 반환
+     * <pre>
+     *     테스트를 위한 메서드
+     * </pre>
+     *
+     * @return 프로모션 개수 반환
+     */
     int getPromotionCount() {
         return promotions.size();
     }
 
+    /**
+     * 프로모션 추가(복수)
+     *
+     * @param lines 프로모션 정보
+     */
     private void addPromotions(List<String> lines) {
         lines.forEach(this::addPromotion);
     }
 
+    /**
+     * 프로모션 추가(단일)
+     *
+     * @param line 프로모션 정보
+     */
     private void addPromotion(String line) {
         List<String> promotionDetail = StringUtil.splitFileString(line);
 
@@ -47,10 +91,20 @@ public class Convenience {
         );
     }
 
+    /**
+     * 제품 추가(복수)
+     *
+     * @param lines 제품 정보
+     */
     private void addProducts(List<String> lines) {
         lines.forEach(this::addProduct);
     }
 
+    /**
+     * 제품 추가(단일)
+     *
+     * @param line 제품 정보
+     */
     private void addProduct(String line) {
         List<String> productDetail = StringUtil.splitFileString(line);
 
@@ -59,6 +113,16 @@ public class Convenience {
         addQuantity(product, Integer.parseInt(productDetail.get(2)), productDetail.get(3));
     }
 
+    /**
+     * 제품 추가를 위한 검색
+     * <pre>
+     *     등록되지 않은 제품이라면 제품을 추가하고 객체 반환
+     * </pre>
+     *
+     * @param name  제품 이름
+     * @param price 제품 가격
+     * @return 제품
+     */
     private Product getProduct(String name, int price) {
         if (!products.containsKey(name)) {
             products.put(name, new CProduct(name, price));
@@ -66,6 +130,16 @@ public class Convenience {
         return products.get(name);
     }
 
+    /**
+     * 제품 추가
+     * <pre>
+     *     프로모션 이름은 null일 경우 기본 제품으로 추가됨
+     * </pre>
+     *
+     * @param product       제품
+     * @param quantity      재고
+     * @param promotionName 프로모션 이름
+     */
     private void addQuantity(Product product, int quantity, String promotionName) {
         if (promotionName.equals("null")) {
             product.addProduct(quantity);
