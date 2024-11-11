070bdf5b5a1f18a91c755395815a14640d1c53d0
yuntasha <jaljayo85@gmail.com>
2024-11-11 20:01:29 +0900

    style(StringUtil): write Javadoc

--- src/main/java/store/util/StringUtil.java
+++ src/main/java/store/util/StringUtil.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class StringUtil {
+    // 기본 문자열 정의
     public static final String LINE_BREAK = "\n";
     public static final String HIGH_BAR = "-";
     public static final String COMMA = ",";
@@ -21,20 +22,55 @@ public class StringUtil {
      * 파일 에러 메세지 형식
      */
     private static final String FILE_ERROR_FORMAT =
-            "%s을 읽어오던 중 문제가 생겼습니다.";
+            "[Error] %s을 읽어오던 중 문제가 생겼습니다.";
+    /**
+     * 재고를 보여줄 때 기본 제품이 출력될 형식
+     */
     private static final String PRODUCT_DETAIL_NO_PROMOTION_FORMAT = "- %s %s원 %s";
+    /**
+     * 재고를 보여줄 때 프로모션 제품이 출력될 형식
+     */
     private static final String PRODUCT_DETAIL_PROMOTION_FORMAT = "- %s %s원 %s %s";
     private static final String NO_QUANTITY = "재고 없음";
     private static final String COUNT = "개";
 
+    /**
+     * 파일 에러 발생시 콘솔에 내보낼 오류
+     * <pre>
+     *     [Error] {파일 이름}을 읽어오던 중 문제가 생겼습니다.
+     * </pre>
+     *
+     * @param s 파일 이름
+     * @return 파일 에러 메세지
+     */
     public static String makeFileErrorMessage(String s) {
         return String.format(FILE_ERROR_FORMAT, s);
     }
 
+    /**
+     * COMMA(,)를 기준으로 분리
+     *
+     * @param s 입력 문자열
+     * @return 분리된 문자열
+     */
     public static List<String> splitFileString(String s) {
         return Arrays.stream(s.split(COMMA)).toList();
     }
 
+    /**
+     * 재고 정보를 형식에 맞게 변환
+     * <pre>
+     *     프로모션 적용된 경우
+     *     - 감자칩 1,500원 5개 반짝할인
+     *     - 감자칩 1,500원 5개
+     *
+     *     프로모션 미적용된 경우
+     *     - 물 500원 10개
+     * </pre>
+     *
+     * @param dto 재고 정보
+     * @return 출력 형식에 맞춘 재고 정보
+     */
     public static String dtoToString(ProductDetailDto dto) {
         StringBuilder result = new StringBuilder();
 
@@ -52,6 +88,7 @@ public class StringUtil {
 
     /**
      * 1,000,000과 같이 변환
+     *
      * @param n 숫자
      * @return 변환된 문자열
      */
@@ -65,6 +102,7 @@ public class StringUtil {
      *     재고가 있는 경우 -> n개
      *     재고가 없는 경우 -> 재고 없음
      * </pre>
+     *
      * @param n 재고
      * @return 형식에 맞는 재고
      */
