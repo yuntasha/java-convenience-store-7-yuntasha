91ddecaee26c6dd09e1f72508f9061c5ef0dd7f7
yuntasha <jaljayo85@gmail.com>
2024-11-11 21:11:36 +0900

    style(COutputView): write Javadoc

--- src/main/java/store/view/COutputView.java
+++ src/main/java/store/view/COutputView.java
@@ -25,16 +25,51 @@ public class COutputView implements OutputView {
         return cOutputView;
     }
 
+    /**
+     * 환영 인사 및 재고 출력
+     * <pre>
+     * 안녕하세요. W편의점입니다.
+     * 현재 보유하고 있는 상품입니다.
+     *
+     * - 콜라 1,000원 7개 탄산2+1
+     * - 콜라 1,000원 10개
+     * - 사이다 1,000원 8개 탄산2+1
+     * - 사이다 1,000원 7개
+     * </pre>
+     * @param productDetailDto 재고 데이터
+     */
     @Override
     public void welcome(List<ProductDetailDto> productDetailDto) {
         System.out.println(makeWelcome(productDetailDto));
     }
 
+    /**
+     * 에러 메세지 반환
+     * @param e 에러
+     */
     @Override
     public void printErrorMessage(ConvenienceException e) {
         System.out.println(e.getConsoleMessage());
     }
 
+    /**
+     * 영수증 출력
+     * <pre>
+     * ==============W 편의점================
+     * 상품명		수량	금액
+     * 콜라		10 	10,000
+     * =============증	정===============
+     * 콜라		2
+     * ====================================
+     * 총구매액		10	10,000
+     * 행사할인			-2,000
+     * 멤버십할인			-0
+     * 내실돈			 8,000
+     * </pre>
+     * @param allProduct 총 구매 제품 출력 메세지
+     * @param promotionProduct 프로모션 할인 출력 메세지
+     * @param total 총 할인 적용 출력 메세지
+     */
     @Override
     public void totalPrint(String allProduct, String promotionProduct, String total) {
         StringJoiner printText = new StringJoiner(StringUtil.LINE_BREAK);
@@ -50,6 +85,17 @@ public class COutputView implements OutputView {
         System.out.println(printText.toString());
     }
 
+    /**
+     * 재고 출력 메세지 반환
+     * <pre>
+     * - 감자칩 1,500원 5개 반짝할인
+     * - 감자칩 1,500원 5개
+     * - 물 500원 10개
+     * - 비타민워터 1,500원 6개
+     * </pre>
+     * @param productDetailDto 재고 데이터
+     * @return 재고 출력 메세지
+     */
     private String makeWelcome(List<ProductDetailDto> productDetailDto) {
         StringJoiner printText = new StringJoiner(StringUtil.LINE_BREAK);
         printText.add(HELLO_MESSAGE);
