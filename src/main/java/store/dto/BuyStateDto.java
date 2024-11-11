24cf59f93ffcea70ba3a620ea6051e0bdd5db9a1
yuntasha <jaljayo85@gmail.com>
2024-11-11 19:16:06 +0900

    style(BuyStateDto): write javadoc

--- src/main/java/store/dto/BuyStateDto.java
+++ src/main/java/store/dto/BuyStateDto.java
@@ -5,23 +5,53 @@ import store.model.BuyState;
 import java.util.Objects;
 
 public class BuyStateDto {
+    /**
+     * 구매 상태
+     * <pre>
+     *     제품을 무료로 받을 수 있는 상태, 일부 제품이 프로모션 미적용 상태, 기본
+     * </pre>
+     */
     private final BuyState buyState;
+    /**
+     * 프로모션 미적용 제품 개수
+     */
     private final int count;
 
+    /**
+     * 일부 제품이 프로모션 미적용인 경우
+     *
+     * @param buyState 구매 상태
+     * @param count    미적용된 제품 개수
+     */
     public BuyStateDto(BuyState buyState, int count) {
         this.buyState = buyState;
         this.count = count;
     }
 
+    /**
+     * 보통 혹은 추가 구매의 경우
+     *
+     * @param buyState 구매 상태
+     */
     public BuyStateDto(BuyState buyState) {
         this.buyState = buyState;
         this.count = 0;
     }
 
+    /**
+     * 구매 상태 반환
+     *
+     * @return 구매 상태
+     */
     public BuyState getBuyState() {
         return buyState;
     }
 
+    /**
+     * 프로모션 미적용된 제품 개수 반환
+     *
+     * @return 프로모션 미적용된 제품 개수
+     */
     public int getCount() {
         return count;
     }
