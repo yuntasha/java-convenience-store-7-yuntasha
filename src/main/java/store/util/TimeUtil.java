dc401c29d761a7b5e5bfee583e57af4887c92b5b
yuntasha <jaljayo85@gmail.com>
2024-11-11 20:02:16 +0900

    style(TimeUtil): write Javadoc

--- src/main/java/store/util/TimeUtil.java
+++ src/main/java/store/util/TimeUtil.java
@@ -5,6 +5,11 @@ import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDate;
 
 public class TimeUtil {
+    /**
+     * 현재 시간 불러옴
+     *
+     * @return 현재 시간
+     */
     public static LocalDate getNowDate() {
         return DateTimes.now().toLocalDate();
     }
