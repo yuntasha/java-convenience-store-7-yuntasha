73b5b3ae445e5f8c6f065569a7738d4656964300
yuntasha <jaljayo85@gmail.com>
2024-11-11 19:33:10 +0900

    style(CPromotion): write javadoc

--- src/main/java/store/model/CPromotion.java
+++ src/main/java/store/model/CPromotion.java
@@ -3,12 +3,39 @@ package store.model;
 import java.time.LocalDate;
 
 public class CPromotion implements Promotion {
+    /**
+     * 프로모션 이름
+     */
     private final String name;
+    /**
+     * N+1 행사에서 N
+     */
     private final int buy;
+    /**
+     * N+1 행사에서 1
+     * <pre>
+     *     2+2와 같은 경우를 위해 만들어 두었지만 이 미션에서 1로 사용
+     * </pre>
+     */
     private final int get;
+    /**
+     * 프로모션 시작 날짜
+     */
     private final LocalDate startDate;
+    /**
+     * 프로모션 마지막 날짜
+     */
     private final LocalDate endDate;
 
+    /**
+     * 프로모션 생성
+     *
+     * @param name      이름
+     * @param buy       N+1 행사에서 N
+     * @param get       N+1 행사에서 1
+     * @param startDate 프로모션 시작 날짜
+     * @param endDate   프로모션 마지막 날짜
+     */
     public CPromotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.buy = buy;
